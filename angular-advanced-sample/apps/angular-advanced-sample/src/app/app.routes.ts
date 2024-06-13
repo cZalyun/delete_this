3cf2c5c8337464639094cd3866f24297dd766f9b
bmago <bmago@cae.com>
2024-06-13 15:03:33 +0200

    feat(signal): created signal sample component

--- angular-advanced-sample/apps/angular-advanced-sample/src/app/app.routes.ts
+++ angular-advanced-sample/apps/angular-advanced-sample/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { SolidSampleComponent } from './solid-sample/solid-sample.component';
+import { SignalSampleComponent } from './signal-sample/signal-sample.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'solid',
+    component: SolidSampleComponent
+  },
+  {
+    path: 'signal',
+    component: SignalSampleComponent
+  }
+];
