985fea1b691bdeb4659a9a36ca00dffeef1f4a19
bmago <bmago@cae.com>
2024-06-28 07:49:04 +0200

    feat(users-data-access): created users-data-access service

--- angular-advanced-sample/libs/users/store/src/lib/state/users.effects.ts
+++ angular-advanced-sample/libs/users/store/src/lib/state/users.effects.ts
@@ -3,18 +3,20 @@ import { createEffect, Actions, ofType } from '@ngrx/effects';
 import { catchError, of, mergeMap, map } from 'rxjs';
 import { UserResponse } from '@angular-advanced-sample/dummy/model';
 import * as UsersActions from './users.actions';
-import { HttpClient } from '@angular/common/http';
+import { UsersService } from 'libs/users/data-access/src/lib/users.service';
 
 @Injectable()
 export class UsersEffects {
   private actions$ = inject(Actions);
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private usersService: UsersService
+  ) {}
 
   init$ = createEffect(() =>
     this.actions$.pipe(
       ofType(UsersActions.initUsers),
-      mergeMap(() => this.http.get<UserResponse>('https://dummyjson.com/users').pipe( // service into data-access -- injection token url
+      mergeMap(() => this.usersService.getData().pipe(
         map((usersResponse: UserResponse) => UsersActions.loadUsersSuccess({users: usersResponse.users}))
       )),
       catchError(error => {
